d2216651c55b30f7fb1aa02b2ddd709a0409daef
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-12-13 12:18:45 -0700

    add bulma

--- deploy/urls.py
+++ deploy/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for dj project.
+URL configuration for deploy project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
