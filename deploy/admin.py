c62e4847c88f3c39d7c676970c95f36eec1c9c9c
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-12-21 21:41:23 -0700

    update admin

--- deploy/admin.py
+++ deploy/admin.py
@@ -34,24 +34,35 @@ class GunicornAdmin(admin.ModelAdmin):
 @admin.register(Postgres)
 class PostgresAdmin(admin.ModelAdmin):
 	fields = []
-	list_display = []
+	list_display = [
+		'__str__',
+	]
 
 @admin.register(Volume)
 class VolumeAdmin(admin.ModelAdmin):
 	fields = []
-	list_display = []
+	list_display = [
+		'__str__',
+	]
 
 @admin.register(Network)
 class NetworkAdmin(admin.ModelAdmin):
 	fields = []
-	list_display = []
+	list_display = [
+		'name',
+		'external',
+	]
 
 @admin.register(EnvironmentVariable)
 class EnvironmentVariableAdmin(admin.ModelAdmin):
 	fields = []
-	list_display = []
+	list_display = [
+		'__str__',
+	]
 
 @admin.register(Label)
 class LabelAdmin(admin.ModelAdmin):
 	fields = []
-	list_display = []
+	list_display = [
+		'__str__',
+	]
