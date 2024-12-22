e3cfa822fb1ceee6809f7bf69eb45dbc9e7ec4c7
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-12-21 18:46:40 -0700

    set DEPLOY_DATA_ROOTs

--- django_multihost/settings/deploy.py
+++ django_multihost/settings/deploy.py
@@ -1,5 +1,9 @@
+from pathlib import Path
+
 DEPLOY_START_PORT = 8000
 
-DEPLOY_GIT_ROOT = ''
-DEPLOY_VOL_ROOT = ''
-DEPLOY_COMPOSE_ROOT = ''
+DEPLOY_DATA_ROOT = Path(__file__).resolve().parent.parent / 'deploydata'
+
+DEPLOY_GIT_ROOT = DEPLOY_DATA_ROOT / 'git'
+DEPLOY_VOL_ROOT = DEPLOY_DATA_ROOT / 'volumes'
+DEPLOY_COMPOSE_ROOT = DEPLOY_DATA_ROOT / 'compose'
