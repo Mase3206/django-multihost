d28805c5506e589a264fb49b958d8f9dcf570ad8
Mase3206 <35052448+Mase3206@users.noreply.github.com>
2024-12-26 16:36:43 -0700

    show stop button if started

--- deploy/static/js/deploy/control.js
+++ deploy/static/js/deploy/control.js
@@ -6,16 +6,27 @@ function start(deploy_id, csrf_token) {
 		dataType: 'json',
 		contentType: "application/json",
 		data: JSON.stringify({
-			"actions": ["start", "stop"]
+			"actions": ["start"]
 		}),
 		beforeSend: xhr => {
 			xhr.setRequestHeader("X-CSRFToken", csrf_token)
 		}
-		// beforeSend: xhr => {
-		// 	xhr.setRequestHeader(
-		// 		"Authorization",
-		// 		"JWT eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzM1MTEyMDA2LCJpYXQiOjE3MzUwMjU2MDYsImp0aSI6ImQ2YzcyMWNhYzIxZDRlNjJiMWMyYzYzMGQ1MzRjMDIwIiwidXNlcl9pZCI6MX0.XWCwILNGI80uA6IhnaJSTuYkH8M87ZmAYeyZJl2TeYA"
-		// 	)
-		// }
+	})
+}
+
+
+function stop(deploy_id, csrf_token) {
+	console.log(`Attempting to stop deployment with ID ${deploy_id}`)
+	$.ajax({
+		url: `/deploy/api/${deploy_id}/`,
+		type: 'PUT',
+		dataType: 'json',
+		contentType: "application/json",
+		data: JSON.stringify({
+			"actions": ["stop"]
+		}),
+		beforeSend: xhr => {
+			xhr.setRequestHeader("X-CSRFToken", csrf_token)
+		}
 	})
 }
